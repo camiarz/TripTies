bb1e71bff91c87ceb6744640261def611851a92f
camila ruiz <camilaruizcruz97@gmail.com>
2024-03-15 05:29:53 -0600

    Changes Swipe

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,5 @@
 class Match < ApplicationRecord
   belongs_to :user1, class_name: 'User'
   belongs_to :user2, class_name: 'User'
-  has_one :chatroom
+  has_one :chatroom, dependent: :destroy
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :user
-  has_many :trip_interests
-  has_many :interests, through: :trip_interests
+  has_many :trip_interests, dependent: :destroy
+  has_many :interests, through: :trip_interests, dependent: :destroy
   validates :destination, :arrival, :departure, presence: true
 end
